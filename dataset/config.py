5f73009a7a119a884d4fa74e26f903e81f93b39c
Dimos Makris <dimos.makris@xlnaudio.com>
2025-01-08 16:49:29 +0200

    upload config

--- dataset/config.py
+++ dataset/config.py
@@ -33,7 +33,7 @@ class Config:
         self.min_distance_to_wall = 1.0  # for mic and source positions
 
         # Augmentations
-        self.aug_factor = 1 # apply augmentations for each slice
+        self.aug_factor = 3 # apply augmentations for each slice
 
         # stft
         self.hop = (
